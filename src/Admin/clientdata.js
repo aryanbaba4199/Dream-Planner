2147dc9c5ef45bd2752bf3b8cb05dd37fa412d85
Aryan Dubey <aryanbaba@gmail.com>
2023-09-24 21:53:21 +0530

    h

--- src/Admin/clientdata.js
+++ src/Admin/clientdata.js
@@ -16,6 +16,7 @@ function AdminPanel() {
       }
       const jsonData = await response.json();
       setData(jsonData);
+      
 
       // Count the number of users
       setUserCount(jsonData.length);
@@ -23,6 +24,7 @@ function AdminPanel() {
       // Get the current date and time as the last updated timestamp
       const currentDate = new Date();
       setLastUpdated(currentDate.toLocaleString());
+      fetchDataAndUpdateState();
     } catch (error) {
       console.error("Error fetching data:", error);
     }
@@ -32,6 +34,7 @@ function AdminPanel() {
   async function deletedata(itemId) {
     try {
       const response = await fetch(`https://dpapi.vercel.app/api/delete/${itemId}`, {
+        // const response = await fetch(`http://localhost:4000/api/delete/${itemId}`, {
         method: 'DELETE',
         headers: {
           'Content-Type': 'application/json',
@@ -61,14 +64,14 @@ function AdminPanel() {
     <>    <div className="cdblank" ></div>
     <div className="dataloader">
       <div className="dataintro">
-        <h1>Dream Planner's Booking Data</h1>
-        <text>Last Updated: {lastUpdated}</text>
+        <h1 className="dream">Dream Planner's Booking Data</h1>
+        <text className="serve">Last Updated: {lastUpdated}</text>
         <text className="total user">Total Users: {userCount}</text>
       </div>
       <div className="dataprovider">
         {data.map((item, index) => (
           <div key={item._id} className="datadiv">
-            <h4>Serial No: {index + 1}</h4>
+            <h4>User: {index + 1}</h4>
             <h4>Name: {item.name}</h4>
             <h6>Mobile: {item.mobile}</h6>
             <h6>Address: {item.address}</h6>
@@ -80,8 +83,8 @@ function AdminPanel() {
               <img
                 src="https://cdn-icons-png.flaticon.com/256/9790/9790368.png"
                 alt="Delete"
-                width='30'
-                height='30'
+                width='40'
+                height='40'
                 onClick={()=> deletedata(item._id)}
               />
               {/* <img
